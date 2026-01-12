3d38d884bad0918d3746e74362c0a8dc84deaec3
nguyenedu <nguyen223610699@lms.utc.edu.vn>
2026-01-12 11:27:50 +0700

    v1

---
+++ truyen-master/src/main/java/com/example/truyen/config/CreateSuperAdmin.java
@@ -0,0 +1,46 @@
+package com.example.truyen.config;
+
+
+
+//Class này được tạo ra khi mà web mới tạo nên chưa có SuperAdmin để quản lý
+
+import com.example.truyen.entity.User;
+import com.example.truyen.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CreateSuperAdmin implements CommandLineRunner {
+
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        boolean hasSuperAdmin = userRepository.findAll().stream()
+                .anyMatch(user -> user.getRole().equals(User.Role.SUPER_ADMIN));
+        if (!hasSuperAdmin) {
+            User superAdmin = User.builder()
+                    .username("superadmin")
+                    .email("superadmin@gmail.com")
+                    .password(passwordEncoder.encode("123456"))
+                    .fullname("Super Admin")
+                    .role(User.Role.SUPER_ADMIN)
+                    .isActive(true)
+                    .build();
+            userRepository.save(superAdmin);
+            log.info("Tài khoản Super Admin đã được tạo thành công");
+            log.info("usernam: superadmin");
+            log.info("password: 123456");
+        }else{
+            log.info(" Đã tồn tại tài khoản SUPER_ADMIN");
+        }
+    }
+}
