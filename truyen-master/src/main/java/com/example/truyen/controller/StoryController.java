931987a72fb4f2fc5a4e0cd10cd0843eb22cc726
nguyenedu <nguyen223610699@lms.utc.edu.vn>
2026-01-14 08:27:42 +0700

    v1

--- truyen-master/src/main/java/com/example/truyen/controller/StoryController.java
+++ truyen-master/src/main/java/com/example/truyen/controller/StoryController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/stories")
 @RequiredArgsConstructor
